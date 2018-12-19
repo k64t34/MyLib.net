1129fae96ba9e71da71d8a5bf84fa93f47c8bd2d
k64t34 <k64t34@gmail.com>
2018-12-19 16:42:59 +0300

    Process.GetParentPID

--- Process.cs
+++ Process.cs
@@ -1,65 +1,29 @@
 //version 1.0
 using System;
+using System.Management;  // <=== Add Reference required!!
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+
 namespace K64t
-{	
-    //[StructLayout(LayoutKind.Sequential)]
-    public struct ParentProcessUtilities
-    {
-        // These members must match PROCESS_BASIC_INFORMATION
-        internal IntPtr Reserved1;
-        internal IntPtr PebBaseAddress;
-        internal IntPtr Reserved2_0;
-        internal IntPtr Reserved2_1;
-        internal IntPtr UniqueProcessId;
-        internal IntPtr InheritedFromUniqueProcessId;
-
-        [DllImport("ntdll.dll")]
-        private static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ParentProcessUtilities processInformation, int processInformationLength, out int returnLength);
-
-        /// <summary>
-        /// Gets the parent process of the current process.
+{
+	class ProcessUtil
+	{
+		/// <summary>
+        /// Gets the parent process id of the given process id.
         /// </summary>
-        /// <returns>An instance of the Process class.</returns>
-        public static Process GetParentProcess()
-        {
-            return GetParentProcess(Process.GetCurrentProcess().Handle);
-        }
-
-        /// <summary>
-        /// Gets the parent process of specified process.
-        /// </summary>
-        /// <param name="id">The process id.</param>
-        /// <returns>An instance of the Process class.</returns>
-        public static Process GetParentProcess(int id)
-        {
-            Process process = Process.GetProcessById(id);
-            return GetParentProcess(process.Handle);
-        }
-
-        /// <summary>
-        /// Gets the parent process of a specified process.
-        /// </summary>
-        /// <param name="handle">The process handle.</param>
-        /// <returns>An instance of the Process class.</returns>
-        public static Process GetParentProcess(IntPtr handle)
-        {
-            ParentProcessUtilities pbi = new ParentProcessUtilities();
-            int returnLength;
-            int status = NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out returnLength);
-            if (status != 0)
-                throw new Win32Exception(status);
-
-            try
-            {
-                return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
-            }
-            catch (ArgumentException)
-            {
-                // not found
-                return null;
-            }
-        }
-    }
+        /// <returns>PID of the Process.</returns>
+        ///
+		public static uint GetParentProcessID(int PID){
+		var query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", PID);
+        var search = new ManagementObjectSearcher("root\\CIMV2", query);
+        var results = search.Get().GetEnumerator();
+        results.MoveNext();
+        var queryObj = results.Current;
+        var parentId = (uint)queryObj["ParentProcessId"];
+        //var parent = Process.GetProcessById((int)parentId);
+        //Console.WriteLine("I was started by {0} PID {1}", parent.ProcessName,parent.Id);
+        //Console.ReadLine();	
+		return parentId;
+		}
+	}
+	
 }
